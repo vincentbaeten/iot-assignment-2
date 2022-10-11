accf63aea1fe0ff5dae480233d7a8b5f0e35807f
Vincent Baeten <vincent@baeten.dev>
2022-10-11 19:51:54 +0200

    Add terraform remote backend exploration

--- main.tf
+++ main.tf
@@ -18,3 +18,36 @@ resource "docker_container" "iot-assignment-2" {
   image = docker_image.iot-assignment-2.image_id
   name  = "iot-assignment-2"
 }
+
+# resource "fakewebservices_vpc" "primary_vpc" {
+#   name       = "Primary VPC"
+#   cidr_block = "0.0.0.0/1"
+# }
+
+# resource "fakewebservices_server" "servers" {
+#   count = 2
+
+#   name = "Server ${count.index + 1}"
+#   type = "t2.micro"
+#   vpc  = fakewebservices_vpc.primary_vpc.name
+# }
+
+# resource "fakewebservices_load_balancer" "primary_lb" {
+#   name    = "Primary Load Balancer"
+#   servers = fakewebservices_server.servers[*].name
+# }
+
+# resource "fakewebservices_database" "prod_db" {
+#   name = "Production DB"
+#   size = 256
+# }
+
+# resource "fakewebservices_server" "server-3" {
+#   name = "Server 3"
+#   type = "t2.macro"
+# }
+
+# resource "fakewebservices_server" "server-4" {
+#   name = "Server 4"
+#   type = "t2.macro"
+# }
